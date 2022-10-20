61e7f1b7a1a0e05d0ccc18f62806697be08fd882
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-10-20 10:13:15 -0400

    more complex client part 1

---
+++ src/main/scala/com/rockthejvm/part9http/examples/ClientApplicationRunner.scala
@@ -0,0 +1,22 @@
+package com.rockthejvm.part9http.examples
+
+import com.rockthejvm.part9http.ziodev.quickstart.users.User
+import zhttp.service.{ChannelFactory, EventLoopGroup}
+import zio._
+
+import java.util.UUID
+
+object ClientApplicationRunner extends ZIOAppDefault {
+
+  val program = {for {
+    _  <- ZIO.logInfo("Starting Client App")
+    maybeUser  <- MoreComplexClient.getUserByUuid(UUID.fromString("e1aed6e0-f92e-4019-85af-02b3c9ad1c86"))
+    _          <- ZIO.logInfo(s"Client runner got $maybeUser")
+    _  <- ZIO.logInfo("Finishing App")
+  } yield ()}.provide(
+    EventLoopGroup.auto(),
+    ChannelFactory.auto
+  )
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = program
+}
--- src/main/scala/com/rockthejvm/part9http/examples/MoreComplexClient.scala
+++ src/main/scala/com/rockthejvm/part9http/examples/MoreComplexClient.scala
@@ -1,8 +1,30 @@
 package com.rockthejvm.part9http.examples
 
+
+import zio._
+import zhttp.service.{ChannelFactory, Client, EventLoopGroup}
+import com.rockthejvm.part9http.ziodev.quickstart.users.User.fromJsonDecoderUser
+import com.rockthejvm.part9http.ziodev.quickstart.users.User
+import zio.json.DecoderOps
+
+import java.util.UUID
+
+
 //Follow example from https://github.com/C2FO/c2fo-scala/blob/develop/c2fo-eligibility/src/main/scala/com/c2fo/eligibility/services/external/CalculatorAPIClient.scala
 // Parse responses from server examples. Json.
 
 object MoreComplexClient {
+
+  val baseUrl = "http://localhost:9090/users"
+
+  // : ZIO[Any,Throwable,User]
+  def getUserByUuid(uuid: UUID): ZIO[EventLoopGroup with ChannelFactory, Serializable, Option[User]] = for {
+    _  <- ZIO.logInfo(s"Fetching user with uuid $uuid")
+    response  <- Client.request(baseUrl+s"/${uuid.toString}")
+    eitherUser       <- response.body.asString.map(_.fromJson[User])
+    user             <- ZIO.fromEither(eitherUser)
+    _  <- ZIO.logInfo(s"Fetched user $user")
+  } yield Option(user)
+
 
 }
