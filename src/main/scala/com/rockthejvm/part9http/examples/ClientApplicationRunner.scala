b7b88c9940b1fdd10049a89abdf3031612f6bf5d
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2022-10-25 10:04:23 -0400

    Adding push request

--- src/main/scala/com/rockthejvm/part9http/examples/ClientApplicationRunner.scala
+++ src/main/scala/com/rockthejvm/part9http/examples/ClientApplicationRunner.scala
@@ -4,14 +4,14 @@ import com.rockthejvm.part9http.ziodev.quickstart.users.User
 import zhttp.service.{ChannelFactory, EventLoopGroup}
 import zio._
 
-import java.util.UUID
-
 object ClientApplicationRunner extends ZIOAppDefault {
 
   val program = {for {
     _  <- ZIO.logInfo("Starting Client App")
-    maybeUser  <- MoreComplexClient.getUserByUuid(UUID.randomUUID())
-    //maybeUser  <- MoreComplexClient.getUserByUuid(UUID.fromString("e1aed6e0-f92e-4019-85af-02b3c9ad1c86"))
+    user       <- ZIO.attempt(User("Alexis",26))
+    createdUserUUID <- MoreComplexClient.createUser(user)
+    _          <- ZIO.logInfo(s"Received uuid $createdUserUUID")
+    maybeUser  <- MoreComplexClient.getUserByUuid(createdUserUUID)
     _          <- ZIO.logInfo(s"Client runner got $maybeUser")
     _  <- ZIO.logInfo("Finishing App")
   } yield ()}.provide(
